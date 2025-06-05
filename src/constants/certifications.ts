908bd9d1deb748565546352a7f696957dc2a2740
dericsadrs <drccsadrs@gmail.com>
2025-06-05 08:55:31 +0800

    refactor: remove courses

--- src/constants/certifications.ts
+++ src/constants/certifications.ts
@@ -6,32 +6,4 @@ export const certifications = [
     credentialId: null,
     url: null
   },
-  {
-    name: 'Cisco Certified Network Associate',
-    issuer: 'Cisco',
-    date: '2024',
-    credentialId: null,
-    url: null
-  },
-  {
-    name: 'Python for Everybody',
-    issuer: 'University of Michigan',
-    date: '2023',
-    credentialId: null,
-    url: null
-  },
-  {
-    name: 'Introduction to Software Engineering',
-    issuer: 'IBM',
-    date: '2023',
-    credentialId: null,
-    url: null
-  },
-  {
-    name: 'Hands-on Introduction to Linux Commands and Shell Scripting',
-    issuer: 'IBM',
-    date: '2023',
-    credentialId: null,
-    url: null
-  }
 ];
